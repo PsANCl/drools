754151ebacdb2c1a507cf4ed2420d31742add401
PsANCl <osb444627724@163.com>
2017-06-09 18:22:14 +0800

    update

---
+++ src/main/java/com/drools/version/five/pojo/DroolsTest.java
@@ -0,0 +1,64 @@
+package com.drools.version.five.pojo;
+
+import org.junit.Test;
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+import junit.framework.TestCase;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+public class DroolsTest extends TestCase {
+
+    public static final void main(String[] args) {
+        try {
+            // load up the knowledge base
+	        KieServices ks = KieServices.Factory.get();
+    	    KieContainer kContainer = ks.getKieClasspathContainer();
+        	KieSession kSession = kContainer.newKieSession("ksession-rules");
+
+            // go !
+            Message message = new Message();
+            message.setMessage("Hello World");
+            message.setStatus(Message.HELLO);
+            kSession.insert(message);
+            kSession.fireAllRules();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    public static class Message {
+
+        public static final int HELLO = 0;
+        public static final int GOODBYE = 1;
+
+        private String message;
+
+        private int status;
+
+        public String getMessage() {
+            return this.message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        public int getStatus() {
+            return this.status;
+        }
+
+        public void setStatus(int status) {
+            this.status = status;
+        }
+
+    }
+    
+    @Test
+    public void test(){
+    }
+
+}
